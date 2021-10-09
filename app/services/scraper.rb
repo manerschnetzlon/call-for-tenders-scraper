11768642e9855572f5e818f7ad27829861085228
Manon Schnetzler <bonsoir@mailo.com>
2021-10-10 00:42:46 +0200

    create scraper service

---
+++ app/services/scraper.rb
@@ -0,0 +1,71 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Scraper
+  def initialize(attributes = {})
+    @url = attributes[:url]
+    @queries = attributes[:queries]
+    @pagination_selectors = attributes[:pagination_selectors]
+    @pagination_regex = attributes[:pagination_regex]
+    @offers_selectors = attributes[:offers_selectors]
+  end
+
+  def call
+    pages_number = self.find_pages_number
+    (1..pages_number).to_a.each do |page|
+      url = @url + @queries.gsub(@queries.match(/\d+/)[0], page.to_s)
+      html_offers_doc = Nokogiri::HTML(open(url).read)
+      offers = html_offers_doc.search(@offers_selectors)
+
+      offers.each do |offer|
+        create_eurolegales_offer(offer) if @url == 'https://www.eurolegales.com'
+        create_centreofficielles_offer(offer) if @url == 'https://www.centreofficielles.com'
+      end
+    end
+  end
+
+  def find_pages_number
+    html_doc = Nokogiri::HTML(open(@url + @queries).read)
+    pages = html_doc.search(@pagination_selectors).map { |page| page.attribute('href').value }.uniq
+    max_pages_number = pages.empty? ? 1 : pages.map { |page| page.match(@pagination_regex)[1].to_i }.max
+    current_page = @queries.match(/\d+/)[0].to_i
+    return current_page if current_page >= max_pages_number
+
+    @queries = @queries.gsub(current_page.to_s, max_pages_number.to_s)
+    self.find_pages_number
+  end
+
+  def create_eurolegales_offer(offer)
+    td_elements = offer.search('td')
+    offer_hash = { reference: td_elements.last.text,
+                   title: td_elements.first.text,
+                   link: "https://www.eurolegales.com#{td_elements.first.search('a').first.attribute('href').value}" }
+
+    html_date = Nokogiri::HTML(open(offer_hash[:link]).read).at('strong:contains("Remise")')
+    unless html_date.nil?
+      date = html_date.next.instance_of?(Nokogiri::XML::Element) ? html_date.next_element : html_date.next
+      offer_hash[:end_date] = Date.strptime(date.text.match(%r{^(\d+\/\d+\/\d+)})[1], '%d/%m/%y') unless date.nil?
+    end
+    create_offer(offer_hash)
+  end
+
+  def create_centreofficielles_offer(offer)
+    link = offer.search('.resultatOrganismeBasTab2 > p > a').first.attribute('href').value
+    date = offer.search('.resultatOrganismeBas').text.match(%r{\d+\/\d+\/\d+})[0]
+    reference = link.match(/_(\d+_\d+).html/)[1]
+    offer_hash = { reference: reference,
+                   title: offer.search('.resultatOrganismeMilieu > p').text,
+                   link: "https://www.centreofficielles.com#{link}",
+                   end_date: Date.strptime(date, '%d/%m/%Y') }
+    create_offer(offer_hash)
+  end
+
+  def create_offer(offer_hash)
+    return if Offer.exists?(reference: offer_hash[:reference])
+
+    Offer.create(reference: offer_hash[:reference],
+                 title: offer_hash[:title],
+                 link: offer_hash[:link],
+                 end_date: offer_hash[:end_date])
+  end
+end
